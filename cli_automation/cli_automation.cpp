5defe8fc08f31157548de3a1a4e57738c8f6c4c5
Alexandru Ramon Miron <60828762+mironalex00@users.noreply.github.com>
2025-04-19 16:55:05 +0200

    refactor: fixed structure and other cli things

--- cli_automation/cli_automation.cpp
+++ cli_automation/cli_automation.cpp
@@ -3,11 +3,32 @@
 #include "cli_api.h"
 #include <windows.h>
 
+/* Callback para teclas normales */
+static void onInput(const Event* ev, void* ud) {
+	if (ev->type != EVENT_INPUT) return;
+	const CliInputEvent* cli_inp_ev = (const CliInputEvent*)ev;
+	println("Has pulsado: %c (%d)", cli_inp_ev->key, (int)cli_inp_ev->key);
+}
+/* Callback para evento de salida */
+static void onExit(const Event* ev, void* ud) {
+	if (ev->type != EVENT_EXIT) return;
+	println("¡Ok!. Limpiando y cerrando...");
+	exit(EXIT_SUCCESS);
+}
 
 //	Main
 int main()
 {
-	printlnerr("Probando err");
-	println("Probando normal");
+	init_utf16_std();
+	Coordinates coords{};
+	ScreenVolume volume = CliGetVolume();
+	CLIMenu mainMenu;
+	coords.x = 5;
+	coords.y = 2;
+	mainMenu.coords = coords;
+	mainMenu.volume = volume;
+	CliAddEventListener(onInput, &mainMenu);
+	CliAddEventListener(onExit, &mainMenu);
+	CliRunMenu(mainMenu);
 	return EXIT_SUCCESS;
 }
